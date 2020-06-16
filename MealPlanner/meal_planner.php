a9c85466cbbec2ba786c11817f4376e101bc191b
OleaBlossom <o.corcoran@gmail.com>
2020-06-17 00:07:23 +0200

    add collapsible div (w/ toggle button) to cards

--- MealPlanner/meal_planner.php
+++ MealPlanner/meal_planner.php
@@ -41,28 +41,43 @@ require 'db_connect.php';
                                     <?php } ?></p>
                             </div>
                             <div class="card-body col-12 border">
-                                <p class="card-title"><?php if ($row['last_eaten']) { ?>
+                                <p>
+                                    <button class="btn btn-outline-info" type="button" data-toggle="collapse" data-target="#meal_<?php echo $row['meal_id'] ?>" aria-expanded="false" aria-controls="collapseExample">
+                                        Ingredients
+                                    </button>
+                                </p>
+                                <div class="collapse" id="meal_<?php echo $row['meal_id'] ?>">
+                                    <div class="card card-body">
+                                        <ul>
+                                            <li>One Ingredient</li>
+                                            <li>Another Thing</li>
+                                            <li>Something Else</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="card-footer text-muted">
+                                    <?php if ($row['last_eaten']) { ?>
                                         last eaten: <?php echo $row['last_eaten'] ?>
                                     <?php } else { ?>
                                         last eaten: never
                                     <?php } ?></p>
+                                </div>
                             </div>
+                            <br>
                         </div>
-                        <br>
-                    </div>
-                <?php  } ?>
-            <?php
+                    <?php  } ?>
+                <?php
             } else {
                 echo "0 results";
             }
             $conn->close();
-            ?>
+                ?>
         </form>
 
-        <!--         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-        <script src="/meal_planning/MealPlanner/js/myscripts.js"></script> -->
+        <!--         <script src="/meal_planning/MealPlanner/js/myscripts.js"></script> -->
 </body>
 
 </html>
